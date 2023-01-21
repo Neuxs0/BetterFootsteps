be10547a32b7a4b9645de917fe5d0db793eb8aac
Neuxs <Neuxsyt@gmail.com>
2023-01-21 05:36:05 -0800

    Add files via upload

---
+++ src/main/java/me/Neuxs/BetterFootsteps/Mod.java
@@ -0,0 +1,18 @@
+package me.Neuxs.BetterFootsteps;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mod implements ModInitializer {
+
+	public static String MOD_ID = "betterfootsteps";
+	
+	public static String ModVer = "Alpha 1";
+	public static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/me/Neuxs/BetterFootsteps/ModClient.java
@@ -0,0 +1,11 @@
+package me.Neuxs.BetterFootsteps;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
